c8d17edc9916f1e984b9fe22ba0144196e50f9f4
Eric Benzschawel <eric.benzschawel@gmail.com>
2015-11-05 21:15:38 -0500

    Map zooming refactor, direction extraction, routing

--- final_wit_test/WitAI/ViewController.swift
+++ final_wit_test/WitAI/ViewController.swift
@@ -1,9 +1,13 @@
 //
 //  ViewController.swift
-//  final_wit_test
+//  BusStop
 //
-//  Created by Eric Benzschawel on 10/22/2015
-//  Copyright (c) 2015 Eric Benzschawel. All rights reserved.
+// Brandeis University COSI 136 Automated Speech Recognition Final Project
+// Project members:
+//   Eric Benzschawel
+//   Adam Berger
+//   Suzanne Blackley
+//   Swini Garimella
 //
 
 import UIKit
@@ -11,15 +15,11 @@ import MapKit
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, WitDelegate {
     
+    
     @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var entityLabel: UILabel!
-    var witButton : WITMicButton?
-    
     @IBOutlet weak var curLocation: UILabel!
-    
     @IBOutlet weak var navMap: MKMapView!
-    var locationManager = CLLocationManager()
-    
     @IBOutlet weak var toggleView: UISegmentedControl!
     
     @IBAction func mapViewChanged(sender: UISegmentedControl) {
@@ -37,70 +37,134 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             navMap.mapType = MKMapType.Standard
         }
     }
-
+    
+    var witButton : WITMicButton?
+    var locationManager = CLLocationManager()
+    var destination : CLLocationCoordinate2D!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         // set the WitDelegate object
         Wit.sharedInstance().delegate = self
         
-        // create the button
+        // create the microphone button
         let screen : CGRect = UIScreen.mainScreen().bounds
         let w : CGFloat = 100
         let rect : CGRect = CGRectMake(screen.size.width/2 - w/2, 40, w, 100)
         witButton = WITMicButton(frame: rect)
         self.view.addSubview(witButton!)
         
+        // request user for location tracking and start tracking
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
         
+        // initialize the map
         navMap.delegate = self
         navMap.showsUserLocation = true
         navMap.showsPointsOfInterest = true
-        navMap.showsCompass = true
+        navMap.showsCompass = true // allow user to re-orient to North if they move the map at all
         
+        // begin tracking the user
+        navMap.setUserTrackingMode(MKUserTrackingMode.Follow, animated: true)
+        
+        // center the map on the user location
+        //let userLocation = CLLocationCoordinate2D(latitude: navMap.userLocation.coordinate.latitude, longitude: navMap.userLocation.coordinate.longitude)
+        navMap.setCenterCoordinate(navMap.userLocation.coordinate, animated: true)
     }
     
+    // witDidGraspIntent
+    // general function essentially unmodified from original source
     func witDidGraspIntent(outcomes: [AnyObject]!, messageId: String!, customData: AnyObject!, error e: NSError!) {
         if ((e) != nil) {
             print("\(e.localizedDescription)")
             return
         }
         
+        // depending on how we build out the decision tree, this code will need to be generalized to each different intent we build out
+        
+        // get the wit.ai results and store to outcomes
         let outcomes : NSArray = outcomes!
         let firstOutcome : NSDictionary = outcomes.objectAtIndex(0) as! NSDictionary
         let text : String = firstOutcome["_text"] as! String
-        //let intent : String = firstOutcome["intent"] as! String
         let entityDict : NSDictionary = firstOutcome["entities"] as! NSDictionary
         let location : NSArray = entityDict["location"] as! NSArray
         let test : NSDictionary = location.objectAtIndex(0) as! NSDictionary
         let entity: String = test["value"] as! String
-       
-        textLabel!.text = text
-        textLabel!.sizeToFit()
         
-        entityLabel!.text = entity
-        entityLabel!.sizeToFit()
+        self.textLabel!.text = text
+        self.textLabel!.sizeToFit()
+        
+        getDestination(entity)
     }
     
+    // locationManager
+    // functions related to the locationManager which is triggered if the user location is updated
+    // output: updates the curLocation field with a string form of the user's current location
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let userLocation = locations[0]
-        curLocation.text = "\(locations[0])"
-        
-        locationManager.stopUpdatingLocation()
-        
-        let location = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
-        
-        let span = MKCoordinateSpanMake(0.05, 0.05)
-        
-        let region = MKCoordinateRegion(center: location, span: span)
-        
-        navMap.setRegion(region, animated: true)
+        self.curLocation.text = "\(locations[0])"
     }
-
+    
+    // getDestionation
+    // input: string -> destination
+    // Takes a destination returned from wit.ai and sets the destination field as this location's coordinate
+    func getDestination(destination : String) {
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(destination, completionHandler: {(placemarks, error) -> Void in
+            if let placemark = placemarks?.first {
+                let destinationCoords: CLLocationCoordinate2D = placemark.location!.coordinate
+                self.destination = destinationCoords
+                self.entityLabel!.text = "\(destinationCoords.latitude), \(destinationCoords.longitude)"
+                self.getDirections(destinationCoords)
+            }
+        })
+    }
+    
+    // getDirections
+    // input: destinatino -> CLLocationCoordinate2D
+    // Takes a coordinate and calculates the directions from the user's current location to the destination coordinates and calls the overlay renderer to render the path on the map
+    func getDirections(destination : CLLocationCoordinate2D) {
+        let request = MKDirectionsRequest()
+        // set the source as the user's current location
+        let source = MKMapItem(placemark: MKPlacemark(coordinate: navMap.userLocation.coordinate, addressDictionary: nil))
+        let destination = MKMapItem(placemark: MKPlacemark(coordinate: destination, addressDictionary: nil))
+        
+        request.source = source
+        request.destination = destination
+        
+        // we'll need to change the transit type to 'Transit' later, but for now this is working
+        request.transportType = MKDirectionsTransportType.Walking
+        
+        // using the first returned route, print the steps to the console and add a map overlay
+        let directions = MKDirections(request: request)
+        directions.calculateDirectionsWithCompletionHandler { response, error in
+            if error == nil {
+                if let r = response {
+                    for step in r.routes[0].steps {
+                        print(step.instructions)
+                    }
+                    // remove any pre-existing overlays
+                    self.navMap.removeOverlays(self.navMap.overlays)
+                    // add the new overlay
+                    self.navMap.addOverlay(r.routes[0].polyline, level: MKOverlayLevel.AboveRoads)
+                }
+            }
+        }
+    }
+    
+    // mapView delegate
+    // takes in an MKOverlay and renders it on the map
+    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        
+        renderer.strokeColor = UIColor.blueColor()
+        renderer.lineWidth = 5.0
+        return renderer
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
